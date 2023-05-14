8d46fee46445d2312323e4c96c1e1fc1d3cf3bff
Assassin17 <souravmalla1998@gmail.com>
2023-05-14 22:11:54 +0100

    Portfolio changed

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -56,14 +56,16 @@ export const SectionId = {
   About: 'about',
   Contact: 'contact',
   Travel-Diary: 'travel-Diary',
-  //Portfolio: 'portfolio'
   Resume: 'resume',
   Skills: 'skills',
   Stats: 'stats',
- // Testimonials: 'testimonials',
 } as const;
-
 export type SectionId = typeof SectionId[keyof typeof SectionId];
+
+
+// Testimonials: 'testimonials',
+//Portfolio: 'portfolio'
+
 
 /**
  * Hero section
