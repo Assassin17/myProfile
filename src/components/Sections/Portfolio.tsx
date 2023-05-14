5fd6403d97ddf8d32d6a65e76d675f5f6e5900e1
Assassin17 <souravmalla1998@gmail.com>
2023-05-14 22:27:10 +0100

    Portfolio changed

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -11,7 +11,7 @@ import Section from '../Layout/Section';
 
 const Portfolio: FC = memo(() => {
   return (
-    <Section className="bg-neutral-800" sectionId={SectionId.Travel-Diary}>
+    <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex flex-col gap-y-8">
         <h2 className="self-center text-xl font-bold text-white">Travel Diary</h2>
         <div className=" w-full columns-2 md:columns-3 lg:columns-4">
@@ -35,8 +35,8 @@ const Portfolio: FC = memo(() => {
   );
 });
 
-Portfolio.displayName = 'Travel-Diary';
-export default Travel-Diary;
+Portfolio.displayName = 'Portfolio';
+export default Portfolio;
 
 const ItemOverlay: FC<{item: PortfolioItem}> = memo(({item: {url, title, description}}) => {
   const [mobile, setMobile] = useState(false);
