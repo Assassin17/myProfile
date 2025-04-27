3d7fe4bdb19f6923f610935c0c6d6bb45e82a623
Assassin17 <souravmalla1998@gmail.com>
2025-04-27 13:34:10 +0100

    Portpolio changes 5.0

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -104,7 +104,7 @@ const Portfolio: FC = memo(() => {
                         <span className={`text-sm font-medium ${
                           status.toLowerCase() === 'going' ? 'text-green-400' :
                           status.toLowerCase() === 'tentative' ? 'text-yellow-400' :
-                          status.toLowerCase() === 'pending' ? 'text-yellow-400' :'text-blue-400'
+                          status.toLowerCase() === 'pending' ? 'text-red-400' :'text-red-400'
                         }`}>
                           Status: {status}
                         </span>
