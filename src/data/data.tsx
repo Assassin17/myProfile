0d1c8378a3107f611ab648ea1bd1567ba2718f13
Assassin17 <souravmalla1998@gmail.com>
2025-06-01 18:13:02 +0100

    updated for timelines

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -28,7 +28,7 @@ import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg'; */
-import profilepic from '../images/profilepic2.jpg';
+import profilepic from '../images/profilepic.jpg';
 //import testimonialImage from '../images/testimonial.webp';
 import {
   About,
